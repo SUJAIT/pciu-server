9711fd0bcf418da3666c338c9083eb29452eb5c4
SUJAIT <sujait2000@.com>
2025-01-02 02:26:55 +0600

    first commit

--- index.js
+++ index.js
@@ -134,8 +134,8 @@ app.post('/create-checkout-session', async (req, res) => {
           payment_method_types: ['card'],
           line_items: lineItems,
           mode: 'payment',
-          success_url: `http://localhost:5173/success`,
-          cancel_url: `http://localhost:5173/cancel`,
+          success_url: `https://busbooking-5d224.web.app/success`,
+          cancel_url: `https://busbooking-5d224.web.app/cancel`,
       });
 
       // Prepare the payment object to save to the database
